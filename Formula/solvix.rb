c40746d2d9c17d2a57c5ad7dd0fe91c4d1558841
Ademuyiwa Afeez <ademuyiwaafeez077@gmail.com>
2026-05-22 13:46:50 +0100

    Publish Solvix 0.3.1 formula

--- Formula/solvix.rb
+++ Formula/solvix.rb
@@ -1,17 +1,17 @@
 class Solvix < Formula
   desc "Computational intelligence layer for developers"
   homepage "https://github.com/celpha2svx/solvix"
-  version "0.2.5"
+  version "0.3.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/celpha2svx/solvix/releases/download/v0.2.5/solvix-macos-x64"
-      sha256 "92f50a2aa78bf6910c8d5cf491ecfdff5419d7201beabc9f3805455c3c20ce6e"
+      url "https://github.com/celpha2svx/solvix/releases/download/v0.3.1/solvix-macos-x64"
+      sha256 "71a9786924a1dbd76dcf33f54d880bd8d106867a6b80dc7db1f9d3358c38f141"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/celpha2svx/solvix/releases/download/v0.2.5/solvix-macos-arm64"
-      sha256 "e688c2f3d2966bf406e4317193db5ac69f6edad14696d9ffa14ee0f1384cf562"
+      url "https://github.com/celpha2svx/solvix/releases/download/v0.3.1/solvix-macos-arm64"
+      sha256 "f93d3850514e5c60e9ddd6b0b71bc9e2530886271608d7ff7bcf393cdbeb9f84"
     end
   end
 
